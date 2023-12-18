55b52c3c0138e4f2732365d75c9a09275fb863da
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-12-18 23:13:41 +0200

    add BinaryFileHandler class

---
+++ src/Classes/BinaryFileHanlder/BinaryFileHandler.java
@@ -0,0 +1,10 @@
+package Classes.BinaryFileHanlder;
+
+public class BinaryFileHandler {
+    public static void write(Object object){
+
+    }
+    public static Object read(){
+        return null ;
+    }
+}
